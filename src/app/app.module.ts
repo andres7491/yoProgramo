5c62312dee58887a1538eb35de16f5caf00aa3aa
andres <andres_grinblat80@hotmail.com>
2022-03-02 20:15:44 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+
+import { AppComponent } from "./app.component";
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaYEducacionComponent } from './componentes/experiencia-y-educacion/experiencia-y-educacion.component';
+import {PorfolioService} from './servicios/porfolio.service';
+import{HttpClientModule} from '@angular/common/http'; 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaYEducacionComponent,
+ 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+  
+    HttpClientModule,
+    
   ],
-  providers: [],
+  
+
+  providers: [PorfolioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/componentes/experiencia-y-educacion/experiencia-y-educacion.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from 'src/app/servicios/porfolio.service';
+
+@Component({
+  selector: 'app-experiencia-y-educacion',
+  templateUrl: './experiencia-y-educacion.component.html',
+  styleUrls: ['./experiencia-y-educacion.component.css']
+})
+export class ExperienciaYEducacionComponent implements OnInit {
+  experienciaList:any;
+  constructor(private datosPorfolio:PorfolioService) { }
+
+  ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos().subscribe(data=>{
+      this.experienciaList=data.experiencia;
+      
+    })
+  
+  
+  
+  
+  }
+
+
+
+  
+
+}
